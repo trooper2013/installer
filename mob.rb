01d1f98e3d3da567a491777eb11114999e2877cd
Emmanuel Texier <emmanuel.texier@magnet.com>
2014-08-24 18:10:10 -0700

    Update mob.rb

--- mob.rb
+++ mob.rb
@@ -10,15 +10,15 @@ end
 def error_invalid_java_version (version) 
 """  
 ERROR: Incorrect Java version #{version}.
-  The Magnet Mobile App builder requires Oracle Java #{JAVA_MIN_VERSION} or above.
+  The Magnet Mobile Generator requires Java #{JAVA_MIN_VERSION} or above.
 """
 end
 
-def error_incorrect_mab(mab_path)
+def error_incorrect_mob(mob_path)
 """
 ERROR: mob executables conflicting on PATH.
-  It appears that you already have a mob executable on your PATH: #{mab_path} 
-  Ensure that /usr/local/bin/mob is in front of PATH to use the installed mab.   
+  It appears that you already have a mob executable on your PATH: #{mob_path} 
+  Ensure that /usr/local/bin/mob is in front of PATH to use the installed mob.   
 """
 end
 
@@ -38,10 +38,10 @@ def check_java
   nil
 end
 
-def check_mab
+def check_mob
  mob = which 'mob'
  if mob 
-   return error_incorrect_mab(mob)
+   return error_incorrect_mob(mob)
  end
  nil
 end
@@ -56,7 +56,7 @@ class JavaDependency < Requirement
   end
 
   satisfy :build_env => false do
-    @error = (check_java ? check_java : "") + (check_java_home ? check_java_home : "") 
+    @error = (check_java ? check_java : "")  
     not (@error != "") 
   end
 
@@ -65,17 +65,11 @@ end
 #
 # MOB formula (devel and stable)
 #
-class Mab < Formula
+class Mob < Formula
   homepage 'http://factory.magnet.com'
-  url "https://raw.githubusercontent.com/magnetsystems/installer/master/magnet-tools-cli-mob-installer-2.3.0-SNAPSHOT.tgz"
+  url "https://raw.githubusercontent.com/magnetsystems/installer/master/magnet-tools-cli-mob-installer-2.3.0-SNAPSHOT.tar.gz"
   sha1 '5d5ed452b0f3243f4981bc1ea25a6d0c7d60202c'
 
-
-  option 'with-mysql', 'MySQL will be installed'
-  option 'without-maven' , 'Maven will not be installed'
-
-  depends_on MavenDependency => :recommended
-  depends_on MySqlDependency => :optional
   depends_on JavaDependency => :recommended
 
   
@@ -85,7 +79,7 @@ class Mab < Formula
 
 
   def caveats 
-    issues = (check_java ? check_java : "") + (check_mab ? check_mab : "")
+    issues = (check_java ? check_java : "") + (check_mob ? check_mob : "")
     if (issues == "") 
       return """NO CAVEATS. INSTALLATION SUCCESSFUL!
   Congratulations! The Mobile Generator has been installed under /usr/local/bin/mob.
